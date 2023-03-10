ca6c902dfb391b3379f67203ce371942388fe2a4
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-03-10 08:03:57 +0400

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,1 +1,17 @@
-
+def read_file():
+    with open('recipes.txt') as file:
+        data = {}
+        for line in file:
+            data[line.strip()] = []
+            count = int(file.readline().strip())
+            for i in range(count):
+                ingredient_name, quantity, measure = file.readline().strip(
+                ).split(' | ')
+                data[line.strip()].append({
+                    'ingredient_name': ingredient_name,
+                    'quantity': int(quantity),
+                    'measure': measure
+                })
+            file.readline()
+    file.close()
+    return data
